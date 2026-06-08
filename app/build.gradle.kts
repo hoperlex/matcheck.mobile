984e80e75028381733fa3db5f672bcd22fad2ff9
adm <adm@PC-86369.mstroy.pro>
2026-06-08 12:54:58 +0300

    release: поднял версию до 1.0.14 (versionCode 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.matcheckmobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 15
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
