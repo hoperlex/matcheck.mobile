78d0e16b5bb8b4a1ab368515caac74cf633ad06e
adm <adm@PC-86369.mstroy.pro>
2026-06-25 18:26:47 +0300

    release: поднял версию до 1.0.23 (versionCode 24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.matcheckmobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.0.22"
+        versionCode = 24
+        versionName = "1.0.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
